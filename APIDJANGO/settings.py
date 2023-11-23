0281058db5ec281e3532ae0ecff8cfab96b6ebf2
TadeoRosales <2019452127@teschi.edu.mx>
2023-11-22 23:55:22 -0600

    api

--- APIDJANGO/settings.py
+++ APIDJANGO/settings.py
@@ -60,8 +60,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # Add the account middleware:
-    "allauth.account.middleware.AccountMiddleware",
+    
 ]
 
 
